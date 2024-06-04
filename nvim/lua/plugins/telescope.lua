9b442faa9155177947625834cce9192dae08e21e
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-06-04 15:32:48 +0100

    Remove unused

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 local opts = function()
 	local actions = require("telescope.actions")
-	local fb_actions = require("telescope._extensions.file_browser.actions")
 
 	return {
 		defaults = {
@@ -17,36 +16,12 @@ local opts = function()
 			},
 			file_ignore_patterns = { "node_modules", "resources/public/js/", ".git/", ".shadow-cljs/" },
 		},
-		extensions = {
-			file_browser = {
-				mappings = {
-					i = {
-						["<Tab>"] = actions.select_default,
-						["<C-CR>"] = fb_actions.create_from_prompt,
-					},
-				},
-			},
-			advanced_git_search = {
-				-- fugitive or diffview
-				diff_plugin = "fugitive",
-				-- customize git in previewer
-				-- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
-				git_flags = {},
-				-- customize git diff in previewer
-				-- e.g. flags such as { "--raw" }
-				git_diff_flags = {},
-			},
-		},
-		extensions_list = { "themes", "terms", "file_browser", "frecency" },
+		extensions_list = { "themes", "terms" },
 	}
 end
 
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-telescope/telescope-file-browser.nvim",
-		"nvim-telescope/telescope-frecency.nvim",
-	},
 	cmd = "Telescope",
 	opts = opts,
 }
