903ab59fb070f03706dbf7f7f04f987c87c6f587
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-06-09 12:28:05 +0100

    Improve toggleterm bindings

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,5 +1,13 @@
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("n", "<esc>", function()
+		local origin_win = require("toggleterm.ui").get_origin_window()
+		vim.api.nvim_win_close(0, true)
+		if vim.api.nvim_win_is_valid(origin_win) then
+			vim.api.nvim_set_current_win(origin_win)
+		end
+	end, opts)
 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
 end
