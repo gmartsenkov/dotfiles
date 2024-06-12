df7af8568ea1ae21031ece271bb523d4fa40173d
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-06-12 13:42:23 +0100

    Improve wezterm tabs style

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,9 +7,10 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.use_fancy_tab_bar = false
 config.enable_wayland = false
 config.color_scheme = "Catppuccin Mocha"
-config.font_size = 16
+config.font_size = 16.5
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "DemiBold" })
 -- config.font = wezterm.font("Victor Mono", { weight = "DemiBold" })
 -- config.font = wezterm.font("Sarasa Fixed CL Nerd Font", { weight = "Medium" })
@@ -17,7 +18,7 @@ config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "DemiBold"
 -- config.font = wezterm.font("UbuntuMono Nerd Font Mono", { weight = "Medium" })
 
 -- let(:teammate) { Teammate.new account: teammate_account }
--- config.cell_width = 0.9
+config.cell_width = 0.95
 config.line_height = 1.0
 
 config.window_decorations = "RESIZE"
@@ -26,56 +27,15 @@ config.initial_cols = 150
 
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
-config.tab_max_width = 22
+config.tab_bar_at_bottom = false
+config.tab_max_width = 50
 
 config.keys = keys
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 0,
+	top = 25,
 	bottom = 0,
 }
-
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 40
-
-local function tab_title(tab_info)
-	local title = tab_info.tab_title
-
-	if title and #title > 0 then
-		return title
-	end
-
-	return tab_info.active_pane.title
-end
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, cf, hover, max_width)
-	local title = tab_title(tab)
-
-	title = wezterm.truncate_left(title, max_width)
-	local i = tab.tab_index + 1
-
-	title = string.format(" %d %s ", i, title)
-
-	local background = "rgb(22, 24, 26 / 20%)"
-	local foreground = "white"
-
-	if tab.is_active then
-		background = "rgb(22, 24, 26 / 90%)"
-		foreground = "white"
-	end
-
-	return {
-		{ Background = { Color = "black" } },
-		{ Foreground = { Color = "black" } },
-		{ Text = "" },
-		{ Background = { Color = background } },
-		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = "black" } },
-		{ Foreground = { Color = "black" } },
-		{ Text = "" },
-	}
-end)
 
 return config
