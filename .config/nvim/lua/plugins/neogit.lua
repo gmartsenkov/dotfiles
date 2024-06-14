695af3197a19f1db87b75d761fb4be0974ede471
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-06-14 13:31:15 +0100

    Lock neogit to older version

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,6 +2,7 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	cmd = "Neogit",
+	commit = "ade9858cb6c434f3642f0b6296ac8d9d4ee51935",
 	config = true,
 	opts = {
 		kind = "tab",
