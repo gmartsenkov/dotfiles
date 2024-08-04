1f2ab3a15563b0810a3709f4b5e2dcbeaed55fa1
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-08-04 17:24:58 +0100

    Update neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,11 +1,16 @@
 return {
 	"NeogitOrg/neogit",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+	},
 	cmd = "Neogit",
-	commit = "ade9858cb6c434f3642f0b6296ac8d9d4ee51935",
 	config = true,
 	opts = {
 		kind = "tab",
 		remember_settings = false,
+		integrations = {
+			fzf_lua = true,
+		},
 	},
 }
