3116a586d45e4e33c403b065285c07dddd435144
gogo <g.martsenkov@gmail.com>
2025-03-26 13:43:54 +0000

    add waybar, nvim tweaks

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -7,20 +7,35 @@ return {
 			local actions = require("telescope.actions")
 			require("telescope").setup({
 				defaults = {
+					preview = false,
+					layout_strategy = "vertical",
+					sorting_strategy = "ascending",
+					layout_config = {
+						prompt_position = "top",
+						width = 0.5,
+						height = 0.5,
+					},
 					mappings = {
 						["i"] = {
 							["<esc>"] = actions.close,
 							["<C-j>"] = actions.move_selection_next,
 							["<C-k>"] = actions.move_selection_previous,
+							["<C-a>"] = { "<home>", type = "command" },
+							["<C-e>"] = { "<end>", type = "command" },
 						},
 					},
 				},
 				extensions = {
 					file_browser = {
+						hidden = { file_browser = true, folder_browser = true },
+						no_ignore = true,
 						-- disables netrw and use telescope-file-browser in its place
 						hijack_netrw = true,
+						hide_parent_dir = true,
 						mappings = {
-							["i"] = {},
+							["i"] = {
+								["<tab>"] = actions.select_default,
+							},
 							["n"] = {},
 						},
 					},
