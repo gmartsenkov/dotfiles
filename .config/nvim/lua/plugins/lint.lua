ed7e335d8ef2b254d11829a86ada82b41ad5487f
gogo <g.martsenkov@gmail.com>
2025-08-13 09:37:33 +0100

    Run rubocop with bundle

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,6 +2,19 @@ return {
 	"mfussenegger/nvim-lint",
 	ft = { "ruby" },
 	config = function()
+		require("lint").linters_by_ft = {
+			ruby = { "rubocop" },
+		}
+		require("lint").linters.rubocop.cmd = "bundle"
+		require("lint").linters.rubocop.args = {
+			"exec",
+			"rubocop",
+			"--format",
+			"json",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
 				require("lint").try_lint()
