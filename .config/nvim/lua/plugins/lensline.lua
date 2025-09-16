91a610b37c443f9161a528fd21f68758af4698d9
Georgi Martsenkov <g.martsenkov@gmail.com>
2025-09-16 02:26:58 +0100

    Configure lensline

--- .config/nvim/lua/plugins/lensline.lua
+++ .config/nvim/lua/plugins/lensline.lua
@@ -2,7 +2,14 @@ return {
 	"oribarilan/lensline.nvim",
 	tag = "1.1.2", -- or: branch = 'release/1.x' for latest non-breaking updates
 	event = "LspAttach",
-	config = function()
-		require("lensline").setup()
-	end,
+	opts = {
+		providers = { -- Array format: order determines display sequence
+			{
+				name = "last_author",
+				enabled = true, -- enabled by default with caching optimization
+				cache_max_files = 1000, -- maximum number of files to cache blame data for (default: 50)
+			},
+		},
+	},
+	config = true,
 }
